f349a027f7e97bd1c0244095498e484821da1279
serene-sparks <186878322+serene-sparks@users.noreply.github.com>
2025-03-08 22:29:00 -0500

    ethan assist!

--- src/js/global.js
+++ src/js/global.js
@@ -1,6 +1,11 @@
 $('.icon,.wooden').hover(function() {
-    var a = Math.random() * 15 - 5;
+    const a = Math.random() * 15 - 5;
     $(this).css('transform', 'rotate(' + a + 'deg) scale(1.07)');
 }, function() {
     $(this).css('transform', 'none');
+});
+
+$(document).ready(function() {
+    const currentYear = new Date().getFullYear();
+    $('#copyright-date').text('est. 2023 - ' + currentYear);
 });
