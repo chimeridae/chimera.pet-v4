eb12a1473b97ca292fb36182cdc0baf806ddc19d
chimeridae <chimeranull@gmail.com>
2025-04-01 20:04:34 -0500

    commission info finished

---
+++ src/js/commcarousels.js
@@ -0,0 +1,42 @@
+ const container = document.getElementById("myCarousel");
+const options = {
+  transition: "slide",
+  hasDots: "false"
+};
+
+
+new Carousel(container, options);
+
+// second
+
+ const container2 = document.getElementById("mobileCarousel");
+const options2 = {
+  transition: "slide",
+  hasDots: "false"
+};
+
+
+new Carousel(container2, options2);
+
+// third
+
+ const container3 = document.getElementById("watercolorfullbody");
+const options3 = {
+  transition: "slide",
+  hasDots: "false",
+};
+
+
+new Carousel(container3, options3);
+
+// four
+
+const container4 = document.getElementById("gbc");
+const options4 = {
+  transition: "slide",
+  hasDots: "false",
+};
+
+
+new Carousel(container4, options4);
+
